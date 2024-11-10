b191a760b1fe8b7109cf65bf907bef412ee1c89d
REZ0N11\rezon <rez0n11@ya.ru>
2024-11-10 10:10:59 +0500

    4.2

---
+++ Tyuiu.RogovAYu.Sprint4.Task1.V21.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.RogovAYu.Sprint4.Task1.V21.Lib
+{
+    public class DataService : ISprint4Task1V21
+    {
+        public int Calculate(int[] array)
+        {
+            int p = 1;
+            for (int i = 0; i < array.Length; i++) { if (array[i]%2==0) { p *= array[i];} }
+            return p;
+        }
+    }
+}
---
+++ Tyuiu.RogovAYu.Sprint4.Task1.V21/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Tyuiu.RogovAYu.Sprint4.Task1.V21.Lib;
+
+namespace Tyuiu.RogovAYu.Sprint4.Task1.V21
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            DataService ds = new DataService();
+            int result;
+            int[] input = new int[11];
+
+            Console.Title = "Task:4.1.v21| Рогов А.Ю., ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Рогов Александр Юрьевич |  ПКТб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:Произведение четных элементов массива[11]                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            for (int i = 0; i <= 10; i++) { input[i] = Convert.ToInt32(Console.ReadLine()); }
+            Console.WriteLine("***************************************************************************");
+
+            { //code
+                result = ds.Calculate(input);
+            }
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"*  = {result}");
+            Console.WriteLine("***************************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
